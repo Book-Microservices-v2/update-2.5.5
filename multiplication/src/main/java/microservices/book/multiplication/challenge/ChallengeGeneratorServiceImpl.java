b6007be8f7e8898d93fe7409c42377d281c2d396
mechero <mechero@users.noreply.github.com>
2021-10-15 10:48:38 +0200

    Updated multiplication service and refactored it to use the new RandomGenerator interface

--- multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeGeneratorServiceImpl.java
@@ -3,6 +3,7 @@ package microservices.book.multiplication.challenge;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 @Service
 public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService {
@@ -10,18 +11,18 @@ public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService 
     private final static int MINIMUM_FACTOR = 11;
     private final static int MAXIMUM_FACTOR = 100;
 
-    private final Random random;
+    private final RandomGenerator randomGenerator;
 
     ChallengeGeneratorServiceImpl() {
-        this.random = new Random();
+        this.randomGenerator = new Random();
     }
 
-    protected ChallengeGeneratorServiceImpl(final Random random) {
-        this.random = random;
+    protected ChallengeGeneratorServiceImpl(final RandomGenerator randomGenerator) {
+        this.randomGenerator = randomGenerator;
     }
 
     private int next() {
-        return random.nextInt(MAXIMUM_FACTOR - MINIMUM_FACTOR) + MINIMUM_FACTOR;
+        return randomGenerator.nextInt(MAXIMUM_FACTOR - MINIMUM_FACTOR) + MINIMUM_FACTOR;
     }
 
     @Override
